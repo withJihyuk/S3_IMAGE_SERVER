1d8ed37a85eabc5f5bbc84d683af6fd7df40f9a5
Jihyuk Lee <lead@sver.dev>
2024-09-07 14:52:03 +0900

    First commit ✨

---
+++ README.md
@@ -0,0 +1,18 @@
+## S3_IMAGE_SERVER
+
+FastAPI + boto3 + PIL로 이루어진 이미지 업로드 서버입니다.   
+엔드포인트로 업로드한 이미지를 .webp 확장자로 변환 하여 s3 버킷에 저장하도록 도와줍니다.
+
+### 사용법
+```
+export S3_ENDPOINT=(본인의 값)
+export S3_ACCESS_KEY=(본인의 값)
+export S3_SECRET_KEY=(본인의 값)
+export S3_REGION=(본인의 값)
+
+pip install -r requirements.txt
+python3 main.py
+```
+
+### To-Do
+- 이미지 이름 랜덤화 및 리턴
---
+++ main.py
@@ -0,0 +1,43 @@
+import io, os, uuid
+import uvicorn
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import JSONResponse
+from PIL import Image
+from boto3 import client
+
+app = FastAPI()
+s3_client = client(
+    "s3",
+    endpoint_url=os.environ.get("S3_ENDPOINT"),
+    aws_access_key_id=os.environ.get("S3_ACCESS_KEY"),
+    aws_secret_access_key=os.environ.get("S3_SECRET_KEY"),
+    region_name=os.environ.get("S3_REGION"),
+)
+
+
+@app.post("/files/images")
+def upload_images(file: UploadFile):
+    max_file_size = 10 * 1024 * 1024  # 10MB
+    file_name = uuid.uuid4().hex
+
+    if file.size > max_file_size:
+        return JSONResponse({"message": "파일 용량이 너무 큽니다."}, status_code=400)
+
+    try:
+        in_mem_file = io.BytesIO()
+        Image.open(file.file).convert("RGBA").save(in_mem_file, "webp", quality=90)
+        in_mem_file.seek(0)
+    except Exception:
+        return JSONResponse({"message": "업로드에 실패 했습니다."}, status_code=500)
+
+    s3_client.put_object(
+        Body=in_mem_file,
+        Bucket="profile-images",
+        Key=f"{file_name}.webp",
+        ContentType="image/*",
+    )
+    return JSONResponse({"result": "success", "file_name": file_name})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
